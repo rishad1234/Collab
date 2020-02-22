051044e0bc63e27d39fc89b996a377f15424df0b
Rishadul Islam <rishadulislamkhan@gmail.com>
2020-02-22 08:14:56 +0600

    updated

---
+++ collab-master/resources/javascript/index.js
@@ -0,0 +1,5 @@
+let window_height = $(window).height();
+let window_width = $(window).width();
+
+$(".landingCover").css("width",window_width);
+$(".landingCover").css("height",window_height- $(".navbar").height()-20);
--- collab-master/webpack.mix.js
+++ collab-master/webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/javascript/app.js', 'public/js')
+   .sass('resources/scss/style.scss', 'public/scss');
