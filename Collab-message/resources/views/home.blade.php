df45f695519426bb16cd613cefef1927a91d45b0
Rishadul Islam <rishadulislamkhan@gmail.com>
2020-03-07 10:42:26 +0600

    credential problem is solved

--- Collab-message/resources/views/home.blade.php
+++ Collab-message/resources/views/home.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="card-body" id="app"></div>
+                    
                 </div>
             </div>
         </div>
