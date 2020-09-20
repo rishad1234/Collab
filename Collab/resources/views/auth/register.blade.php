92a951ddb9d95fc0edd31d2acbb89ea73137a12b
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2020-09-20 23:45:54 +0600

    Register and Login Frontend Update

--- Collab/resources/views/auth/register.blade.php
+++ Collab/resources/views/auth/register.blade.php
@@ -46,7 +46,7 @@
                         }
                     } ?>
                     <div class="form-group">
-                        <label for="InputEmail">Institutional Email address</label>
+                        <label for="InputEmail">Email address</label>
                         <input id="InputEmail" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                         @error('email')
